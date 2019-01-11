512740bc84d8a9de068813d319cb75b2f3e50183
pisarenko91 <nikola.jovanovic991@gmail.com>
2019-01-11 23:04:58 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import { Provider } from "react-redux";
+import store from "./store";
+import Results from "./Results";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+            <Link to="/search-params">
+              <span aria-label="search" role="img">
+                🔍
+              </span>
+            </Link>
+          </header>
+          <Router>
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
+          </Router>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
---
+++ src/SearchBox.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { ANIMALS } from "petfinder-client";
+import { connect } from "react-redux";
+import getBreeds from "./actionCreators/getBreeds";
+import changeLocation from "./actionCreators/changeLocation";
+import changeAnimal from "./actionCreators/changeAnimal";
+import changeBreed from "./actionCreators/changeBreed";
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div className="search-params">
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            onChange={this.props.handleLocationChange}
+            value={this.props.location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={this.props.animal}
+            onChange={this.props.handleAnimalChange}
+            onBlur={this.props.handleAnimalChange}
+          >
+            <option />
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!this.props.breeds.length}
+            id="breed"
+            value={this.props.breed}
+            onChange={this.props.handleBreedChange}
+            onBlur={this.props.handleBreedChange}
+          >
+            <option />
+            {this.props.breeds.map(breed => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button onClick={this.props.search}>Submit</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ breed, breeds, animal, location }) => ({
+  breed,
+  breeds,
+  location,
+  animal
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleAnimalChange(event) {
+    dispatch(changeAnimal(event.target.value));
+    dispatch(getBreeds());
+  },
+  handleBreedChange(event) {
+    dispatch(changeBreed(event.target.value));
+  },
+  handleLocationChange(event) {
+    dispatch(changeLocation(event.target.value));
+  }
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
